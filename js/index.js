caaa2ae5bfef2ea33f9b28ec9324d8ee6726a959
BuildTools <unconfigured@null.spigotmc.org>
2023-05-08 05:12:23 +0300

    firebase connection, webpack setup and reorganize

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+// import firebase libraries
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
   "use strict";
@@ -56,5 +62,33 @@ TweenMax.from(".login-form", 1, {
   ease: Expo.easeInOut,
 });
 
+// setup connection to firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCvix8gWSjyc5qZvdwAiz4fNlkgUclPy1s",
+  authDomain: "internship-automation-process.firebaseapp.com",
+  projectId: "internship-automation-process",
+  storageBucket: "internship-automation-process.appspot.com",
+  messagingSenderId: "884525502818",
+  appId: "1:884525502818:web:199d2dbb5c1fd87c101c3b"
+}
+// init firebase
+initializeApp(firebaseConfig);
+// init services
+const db = getFirestore();
+// collection refs
+const colRef = collection(db, 'users');
+// fetch from database
+getDocs(colRef)
+  .then((snapshot) => {
+    let names = []
+    snapshot.docs.forEach((doc) => {
+      names.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(names)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
 
+console.log('hello?');
 
